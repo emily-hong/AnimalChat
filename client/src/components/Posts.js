6a71ca7f47f3e253e3af7fd27a7e5dba010cb01d
이상아 <lebikebihebi@gmail.com>
2021-10-08 17:30:46 +0900

    feat: 게시글 모음 컴포넌트(Posts)에 그리드 적용 성공

--- client/src/components/Posts.js
+++ client/src/components/Posts.js
@@ -1,23 +1,29 @@
 import SinglePostOnBoard from "./SinglePostOnBoard";
 
 export default function Posts() {
-  const sectionStyle = {
-    display: 'inline-flex',
+  const outerStyle = {
+    backgroundColor: '#D9D9D9'
+  }
 
+  const innerCoreStyle = {
+    display: 'grid',
+    gap: '1.5rem',
+    gridTemplateColumns: 'repeat(auto-fit, 200px)',
+    justifyContent: 'center'
   }
 
   return (
-    <div className="postsSection" style={sectionStyle}>
-      <SinglePostOnBoard />
-      <SinglePostOnBoard />
-      <SinglePostOnBoard />
-      <SinglePostOnBoard />
-      <SinglePostOnBoard />
-      <SinglePostOnBoard />
-      <SinglePostOnBoard />
-      <SinglePostOnBoard />
-      <SinglePostOnBoard />
-      <SinglePostOnBoard />
+    <div className="postsOuter" style={outerStyle}>
+      <div className="postsInnerCore" style={innerCoreStyle}>
+        <SinglePostOnBoard />
+        <SinglePostOnBoard />
+        <SinglePostOnBoard />
+        <SinglePostOnBoard />
+        <SinglePostOnBoard />
+      </div>
     </div>
   );
 }
+
+// 게시물 자료 배열에 map으로 <SinglePostOnBoard /> 적용
+// 게시물이 칸 맞춰서 배치되면 좋겠는데 잘 안 되어서 진도가 안 나감...
--- client/src/components/SinglePostOnBoard.js
+++ client/src/components/SinglePostOnBoard.js
@@ -2,26 +2,29 @@
 export default function SinglePostOnBoard() {
   // css styles
   const flex = { display: 'flex' }
-  const flexColumn = { flexDirection: 'column' }
+  // const flexColumn = { flexDirection: 'column' }
+  const grid = { display: 'grid' }
   const borderRed = { border: '1px solid red' }
   const mockBgColorGreen = { backgroundColor: '#32a852' }
   const mockBgColorBlue = { backgroundColor: '#4976bf' }
-  const width250 = { width: '250px'}
-  const height350 = { height: '350px' }
+  const width200 = { width: '200px'}
+  const height300 = { height: '300px' }
 
   return (
-    <div className="singlePostOnBoard" style={{...flex, ...flexColumn, ...borderRed, ...width250, ...height350}}>
-      <div className="thumbnail" style={{...mockBgColorGreen, height:'250px'}}>
+    <div className="singlePostOnBoard" style={{...borderRed, ...width200, ...height300, ...grid}}>
+      <div className="thumbnail" style={{...mockBgColorGreen, height:'200px'}}>
         {'사진 미리보기'}
       </div>
-      <div className="titlePreview" style={{...flex, ...borderRed, height: '80px'}}>
+      <div className="titlePreview" style={{...flex ,...borderRed, height: '60px'}}>
         <span className="profilePicture" style={{...mockBgColorBlue}}>
           {'프로필사진'}
         </span>
-        <span className="writer">
-          {'김코딩:'}
-        </span>
-        <span className="title">{'만나서 반갑습니다.'}</span>
+        <div className="text">
+          <span className="writer">
+            {'김코딩:'}
+          </span>
+          <span className="title">{'만나서 반갑습니다.'}</span>
+        </div>
       </div>
     </div>
   );
