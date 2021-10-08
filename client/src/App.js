b8ec4e4dd10cd95278d4bfff11bef03ac618265c
이상아 <lebikebihebi@gmail.com>
2021-10-08 11:28:26 +0900

    feat: 메인페이지에서 보이는 게시글 미리보기 컴포넌트 제작

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import React, { useState } from "react"
-import { Switch, Route, useHistory, Redirect } from "react-router-dom"
+// import { Switch, Route, useHistory, Redirect } from "react-router-dom"
 import Header from "./components/Header"
+import SinglePostOnBoard from "./components/SinglePostOnBoard"
 
 function App() {
+  // testing
   return (
     <div className="App">
       <Header />
+      <SinglePostOnBoard />
     </div>
   )
 }
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,15 +1,12 @@
-export default function Header(props) {
+export default function Header() {
+  // css styles
   const borderRed = { border: '1px solid red' };
-
   const flex = { display: 'flex' };
-
   const flexColumn = { flexDirection: 'column' };
-
   const alignCenter = {
     justifyContent: 'center',
     alignItems: 'center'
   };
-
   const alignLeft = {
     justifyContent: 'flex-end',
     alignItems: 'center'
@@ -17,19 +14,19 @@ export default function Header(props) {
 
   return (
   <div className="header" style={{...flex, ...flexColumn}}>
-    <div className="header-buttons" style={{...flex,...alignLeft, ...borderRed}}>
+    <div className="headerButtons" style={{...flex,...alignLeft, ...borderRed}}>
       {/* 링크 연결 : 마이페이지, 로그아웃*/}
-      <button className="header-top-buttons">마이페이지</button>
-      <button className="header-top-buttons">로그아웃</button>
+      <button className="headerTopButtons">마이페이지</button>
+      <button className="headerTopButtons">로그아웃</button>
     </div>
 
-    <div className="header-title-desc" style={{...flex, ...flexColumn, ...alignCenter, ...borderRed}}>
+    <div className="headerTitleDesc" style={{...flex, ...flexColumn, ...alignCenter, ...borderRed}}>
       {/* header-title에 메인 페이지 링크 연결 */}
-      <h1 className="header-title" style={{...borderRed}}>Animal Chat🐱</h1>
-      <h2 className="header-desc" style={{...borderRed}}>반려동물 집사 커뮤니티</h2>
+      <h1 className="headerTitle" style={{...borderRed}}>Animal Chat🐱</h1>
+      <h2 className="headerDesc" style={{...borderRed}}>반려동물 집사 커뮤니티</h2>
     </div>
 
-    <div className="header-boards" style={{...flex, ...alignLeft, ...borderRed}}>
+    <div className="headerBoards" style={{...flex, ...alignLeft, ...borderRed}}>
       {/* 게시판별 링크 연결 */}
       <button className="board">햄스터</button>
       <button className="board">병아리</button>
