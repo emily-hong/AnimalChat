811019e9dde8f01111f00e170adea50030f8bbf7
이상아 <lebikebihebi@gmail.com>
2021-10-08 11:53:10 +0900

    feat: 네비게이션 컴포넌트에 글쓰기 버튼 표시용 props 지정

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -3,26 +3,35 @@ export default function Navigation({ isWriteButton }) {
   // css styles
   const borderRed = { border: '1px solid red' };
   const flex = { display: 'flex' };
-  const alignLeft = {
-    justifyContent: 'flex-end',
-    alignItems: 'center'
-  };
+  const seperateButtonGroups = { justifyContent: 'space-between' }
+  // const alignLeft = {
+  //   justifyContent: 'flex-end',
+  //   alignItems: 'center'
+  // };
+  // const alignRight = {
+  //   justifyContent: 'flex-start',
+  //   alignItems: 'center'
+  // };
 
   return (
     <div className="navigation">
-      <div className="headerBoards" style={{...flex, ...alignLeft, ...borderRed}}>
-        {/* 게시판별 링크 연결 */}
-        {
-          isWriteButton ?
-            <button className="board">글쓰기</button>
-          :
-            null
-        }
-        <button className="board">햄스터</button>
-        <button className="board">병아리</button>
-        <button className="board">앵무새</button>
-        <button className="board">토끼</button>
-        <button className="board">고슴도치</button>
+      <div className="headerBoards" style={{...flex, ...seperateButtonGroups, ...borderRed}}>
+        <div className="writeButtonSpace">
+          {
+            isWriteButton ?
+              <button className="board writeButton">글쓰기</button>
+            :
+              null
+          }
+        </div>
+
+        <div className="animalBoards">
+          <button className="boardButton">햄스터</button>
+          <button className="boardButton">병아리</button>
+          <button className="boardButton">앵무새</button>
+          <button className="boardButton">토끼</button>
+          <button className="boardButton">고슴도치</button>
+        </div>
       </div>
     </div>
   );
