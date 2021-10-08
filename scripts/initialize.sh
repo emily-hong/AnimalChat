d8dbc0b82176ecc567cfd9edcf9b6d1e97d46e84
seonhwa oh <seonhwa_0713@naver.com>
2021-10-08 16:21:57 +0900

    Feat:서버라우트, 로그인, 회원가입 및 initialize.sh 캐시 파일 추가

--- scripts/initialize.sh
+++ scripts/initialize.sh
@@ -3,6 +3,12 @@ cd /home/ubuntu/AnimalChat/server
 npm install
 npm install pm2@latest -g
 sudo apt-get update
+
+sudo npm cache clean --force
+sudo npm install -g n
+sudo n stable
+sudo npm install -g npm
+
 sudo apt-get install authbind
 sudo touch /etc/authbind/byport/80
 sudo chown ubuntu /etc/authbind/byport/80
