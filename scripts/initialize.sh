cfce051c6e6229617a7b62bb2fe95d6ff9a80c09
seonhwa oh <seonhwa_0713@naver.com>
2021-10-08 16:35:32 +0900

    Remove:initialize.sh 캐시 명령어 삭제

--- scripts/initialize.sh
+++ scripts/initialize.sh
@@ -3,12 +3,6 @@ cd /home/ubuntu/AnimalChat/server
 npm install
 npm install pm2@latest -g
 sudo apt-get update
-
-sudo npm cache clean --force
-sudo npm install -g n
-sudo n stable
-sudo npm install -g npm
-
 sudo apt-get install authbind
 sudo touch /etc/authbind/byport/80
 sudo chown ubuntu /etc/authbind/byport/80
