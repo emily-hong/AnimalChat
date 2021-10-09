efe6bf410ae794d671098c40367c70ef32ae93f4
kingshuny <nicecap77@gmail.com>
2021-10-09 19:34:35 +0900

    get요청12 테스트

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(
   cors({
-    origin: ["https://animal-chat.vercel.app/"],
+    origin: ["http://animal-chat.vercel.app/"],
     credentials: true,
     methods: ["GET", "POST", "OPTIONS"],
   })
