c336cf63a56199108d811bde8e60065ace399484
kingshuny <nicecap77@gmail.com>
2021-10-10 13:30:58 +0000

    변경사항이 서버에 올라갈지

--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
-require('dotenv').config();
-const fs = require('fs');
-const https = require('https');
-const cors = require('cors');
-const cookieParser = require('cookie-parser');
-const express = require('express');
-const app = express();
-const controllers = require('./controllers');
+require("dotenv").config()
+const fs = require("fs")
+const https = require("https")
+const cors = require("cors")
+const cookieParser = require("cookie-parser")
+const express = require("express")
+const app = express()
+const controllers = require("./controllers")
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
@@ -15,14 +15,14 @@ app.use(
     credentials: true,
     methods: ["GET", "POST", "OPTIONS"],
   })
-);
+)
 
 app.use(cookieParser())
 
 //test
-app.get('/', (req, res) => {
-  res.status(201).send('Hello World');
-});
+app.get("/", (req, res) => {
+  res.status(201).send("Hello World2")
+})
 
 // app.post('/commentsend', controllers.commentsend)
 // app.post('/postedit', controllers.postedit)
@@ -41,16 +41,16 @@ app.get('/', (req, res) => {
 // app.delete('/postdelete', controllers.postdelete)
 // app.delete('/userremove', controllers.userremove)
 
+const HTTPS_PORT = process.env.HTTPS_PORT || 80
 
-const HTTPS_PORT = process.env.HTTPS_PORT || 80;
-
-let server;
-if (fs.existsSync('./key.pem') && fs.existsSync('./cert.pem')) {  // https 프로토콜 사용 시
-  const privateKey = fs.readFileSync(__dirname + '/key.pem', 'utf8');
-  const certificate = fs.readFileSync(__dirname + '/cert.pem', 'utf8');
-  const credentials = { key: privateKey, cert: certificate };
-  server = https.createServer(credentials, app);
-  server.listen(HTTPS_PORT, () => console.log('https server runnning'));
+let server
+if (fs.existsSync("./key.pem") && fs.existsSync("./cert.pem")) {
+  // https 프로토콜 사용 시
+  const privateKey = fs.readFileSync(__dirname + "/key.pem", "utf8")
+  const certificate = fs.readFileSync(__dirname + "/cert.pem", "utf8")
+  const credentials = { key: privateKey, cert: certificate }
+  server = https.createServer(credentials, app)
+  server.listen(HTTPS_PORT, () => console.log("https server runnning"))
 } else {
   server = app.listen(HTTPS_PORT, () => console.log("http server runnning"))
 }
