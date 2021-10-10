07858a71d93621ba08e17665638d9cfee6f39317
이상아 <lebikebihebi@gmail.com>
2021-10-11 00:05:50 +0900

    Feat: onChange 핸들러 함수 추가

--- client/src/pages/11-2.pwdSection.js
+++ client/src/pages/11-2.pwdSection.js
@@ -1,27 +1,81 @@
+import { useState } from 'react';
 import styled from 'styled-components'
 
-// const StyledInput
+const StyledPwdChangeSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid red;
+`;
+
+const StyledPwdInputsArea = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+const StyledPwdArea = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  // & > p {
+  //   font-weight: bold;
+  // }
+`;
 
 export default function PasswordChange() {
+  // states
+  const [ inputs, setInputs ] = useState({
+    curPwd: "",
+    newPwd: ""
+  })
+  const { curPwd, newPwd } = inputs;
+
+  // input onChange event
+  const handleOnChange = (e) => {
+    setInputs({
+      ...inputs,
+      [e.target.name] : e.target.value
+    });
+  }
+
+  // button event
+  const handleButtonClick = (e) => {
+    e.preventDefault();
+    console.log('비밀번호 변경 버튼 동작 확인')
+  }
+
   return (
     <div className="passwordChangeComponent" style={{border: '1px solid red'}}>
-      <div>
-        <span>현재 비밀번호</span>
-        <input
-          type='password'
-          name='oldPassword'
-        />
-      </div>
-      <div>
-        <span>새 비밀번호</span>
-        <input
-          type='password'
-          name='newPassword'
-        />
-      </div>
-      <div>
-        <button>확인</button>
-      </div>
+      <StyledPwdChangeSection>
+        <StyledPwdInputsArea>
+          <StyledPwdArea>
+            <p>현재 비밀번호</p>
+            <input
+              type='password'
+              name='curPwd'
+              placeholder='현재 비밀번호를 입력하세요'
+              value={curPwd}
+              onChange={handleOnChange}
+            />
+          </StyledPwdArea>
+          <StyledPwdArea>
+            <p>새 비밀번호</p>
+            <input
+              type='password'
+              name='newPwd'
+              placeholder='원하는 비밀번호를 입력하세요'
+              value={newPwd}
+              onChange={handleOnChange}
+            />
+          </StyledPwdArea>
+        </StyledPwdInputsArea>
+        <div>
+          <button onClick={handleButtonClick}>확인</button>
+        </div>
+      </StyledPwdChangeSection>
     </div>
   );
 }
