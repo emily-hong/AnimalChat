b358f3d8be2f09da346b8ba49febffd5a7a51568
이상아 <lebikebihebi@gmail.com>
2021-10-10 20:06:07 +0900

    Refactor: Navigation 컴포넌트에 styled-component 적용

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -1,19 +1,25 @@
 import { Link } from 'react-router-dom'
+import styled from 'styled-components'
 
 export default function Navigation() {
-  // css styles
-  const borderRed = { border: '1px solid red' };
-  const flex = { display: 'flex' };
-  const alignRight = { justifyContent: 'flex-end' };
+  const ButtonsArea = styled.div`
+    display: flex;
+    justify-content: flex-end;
+    border: 1px solid red;
+  `;
+
+  const HomeButtonArea = styled.div`
+    margin-right: 2rem; 
+  `;
 
   return (
     <div className="navigation">
-      <div className="pageButtons" style={{...flex, ...alignRight, ...borderRed}}>
-        <div className="homeButtonSpace">
+      <ButtonsArea>
+        <HomeButtonArea>
           <Link to="/board">
             <button className="homeButton">Home</button>
           </Link>
-        </div>
+        </HomeButtonArea>
         <div className="boardButtonsSpace">
           <Link to="/board/hamster">
             <button className="boardButton">햄스터</button>
@@ -31,11 +37,11 @@ export default function Navigation() {
             <button className="boardButton">고슴도치</button>
           </Link>
         </div>
-      </div>
+      </ButtonsArea>
     </div>
   );
 }
 
 // TODO
 // [x] react-router-dom 적용 필요
-// 홈버튼과 게시판 버튼 그룹 사이에 공간 필요
+// [x] 홈버튼과 게시판 버튼 그룹 사이에 공간 필요 // margin으로? (o)
