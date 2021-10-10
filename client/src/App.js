29c4046159debd6ccd680e7e3238281fda3a7c3d
이상아 <lebikebihebi@gmail.com>
2021-10-10 23:16:50 +0900

    Chore: pull request 준비

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,65 @@
-import React from "react"
-import "./App.css"
-import MainPage from "./pages/4-2.mainPage"
+import React, { useState } from "react"
+import { Switch, Route} from "react-router-dom"
+// import axios from "axios"
+import NavBar from "./pages/0.navBar"
+// import FirstPage from "./pages/1.firstPage"
+import Signup from "./pages/2.signUp"
+// import MainPage from "./pages/4.mainPage"
+import PostRead from "./pages/8.postRead"
 
-export default function App() {
+import "./App.css"
+
+function App() {
+  // /// 서버배포정상 완료시 테스트 코드
+  // const [isLogin, setIsLogin] = useState(false)
+  // function onclickButton() {
+  //   axios
+  //     .get("http://localhost:4001/", { withCredentials: true })
+  //     .then(function (response) {
+  //       setIsLogin(true)
+  //     })
+  // }
+  // return (
+  //   <div className="App">
+  //     <button onClick={onclickButton}> 버튼 </button>
+  //     {isLogin ? "hello world" : "welcome hell world :)"}
+  //   </div>
+  // )
+  // ///
+
+  // const [isLogin, setIsLogin] = useState(false)
+
+  // 회원가입
+  // https://animalchat/signup
+
   return (
     <>
-      <MainPage />
+      <NavBar />
+      <div className='entire'>
+
+        <Switch>
+          <Route path='/signup'>
+            <Signup />
+          </Route>
+
+          <Route path='/postread'>
+            <PostRead />
+          </Route>
+
+          {/* <Route path="/firstpage">
+            <FirstPage />
+          </Route> */}
+
+          {/* <Route exact path="/mainpage">
+            <MainPage />
+          </Route> */}
+
+          {/* <Route path="/">
+            {isLogin ? <Redirect to="/firstpage" /> : <Redirect to="/mainpage" />}
+          </Route> */}
+
+        </Switch>
+      </div>
     </>
   );
 }
