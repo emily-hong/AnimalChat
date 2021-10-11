83c64fc2ce973ceb98ae0a7397115cb84a57522c
이상아 <lebikebihebi@gmail.com>
2021-10-11 18:06:37 +0900

    Style: 페이지 분리, App.js 수정, 렌더링 확인

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import MainPage from './pages/4-2.mainPage'
+import Hamster from './pages/5-1.hamster'
+import Chick from './pages/5-2.chick'
+import Parrot from './pages/5-3.parrot'
+import Rabbit from './pages/5-4.rabbit'
+import Hedgehog from './pages/5-5.hedgehog'
+
+export default function App () {
+  return (
+    <div>
+      <MainPage />
+      <Hamster />
+      <Chick />
+      <Parrot />
+      <Rabbit />
+      <Hedgehog />
+    </div>
+  );
+}
