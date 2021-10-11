d495abbddb4af5d3f77435b9ae9ff9289b6c7e3f
emily-hong <emilyhong4659@gmail.com>
2021-10-12 02:02:08 +0900

    Feat:client,server수정

--- server/migrations/20211011065346-create-post.js
+++ server/migrations/20211011065346-create-post.js
@@ -8,7 +8,7 @@ module.exports = {
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      post_tile: {
+      post_title: {
         type: Sequelize.STRING
       },
       post_content: {
