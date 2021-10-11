f2a44f45633ac68c758f4a8e1565a452b344abf5
emily-hong <emilyhong4659@gmail.com>
2021-10-12 02:45:06 +0900

    server/postedit수정중

--- server/controllers/users/postedit.js
+++ server/controllers/users/postedit.js
@@ -1,3 +1,14 @@
+const { post } = require("../../models")
+const { generateAccessToken, generateRefreshToken } = require("../tokenFunc")
+
 module.exports = (req, res) => {
-    res.send()
+    // 기존게시물 ->, 새로 작성한 게시물 데이터로 업데이트
+    console.log(req.body)
+    const {post_title, post_content} = req.body
+
+    if( !post_title || !post_content ){
+        res.status(401).send('제목과 내용은 필수입니다.')
+    }else{
+        
+    }
 };
