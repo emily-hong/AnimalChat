d38ddbe1914d6463c15a8a608cc7355355ceca6f
seonhwa oh <seonhwa_0713@naver.com>
2021-10-12 02:06:06 +0900

    Chore:중간저장

--- server/controllers/tokenFunc/index.js
+++ server/controllers/tokenFunc/index.js
@@ -5,34 +5,27 @@ module.exports = {
     generateAccessToken: (data) => {
         return sign(data, process.env.ACCESS_SECRET, { expiresIn: '2days' })
     },
-    generateRefreshToken: (data) => {
-        return sign(data, process.env.REFRESH_SECRET, { expiresIn: '5days' })
+
+    sendAccessToken: (res, accessToken, data) => {
+        res.cookie('jwt', accessToken, { HttpOnly: true })
+        res.status(200).json({ data: { accessToken: accessToken, data: data }, message: 'ok' })
     },
-    sendRefreshToken: (data) => {
-        res.cookie('refreshToken', refreshToken, {
-        HttpOnly: true 
-        })
-    },
-    sendAccessToken: (res, accessToken) => {
-        res.json({ data: { accessToken }, message: 'ok' })
-    },
-    resendAccessToken: (res, accessToken) => {
-        res.json({ accessToken, message: 'ok' })
-    },
+    
 
    isAuthorized: (req) => {
      //로그인하는 토큰이 아직 제대로 잘 넘어오는지 아직 모름 
-    //  const authorization = req.headers['Authorization'];
-    //  if(!authorization){
-    //     return null;
-    //  }
-    //  //어떻게 생겼니 ㅠㅠㅠ 일단 아래 방법으로 잘라봄,,
-    //  const token = authorization.split(' ')[1];
-    //  try {
-    //     return verify(token, process,env.ACCESS_SECRET) 
-    //  }catch(err){
-    //     return null; 
-    //  }
+     console.log(req.headers)
+     const authorization = req.headers['Authorization'];
+     if(!authorization){
+        return null;
+     }
+
+     const token = authorization.split(' ')[1];
+     try {
+        return verify(token, process,env.ACCESS_SECRET) 
+     }catch(err){
+        return null; 
+     }
    },
 
    checkRefreshToken: (refreshToken) => {
