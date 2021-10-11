7151b3d52d406f1d13c2f56c768772bea406e586
이상아 <lebikebihebi@gmail.com>
2021-10-11 10:46:02 +0900

    Chore: 최신버전의 origin/dev 브랜치를 pull

--- client/src/App.js
+++ client/src/App.js
@@ -1,46 +1,59 @@
-import React, { useState } from "react"
-import { Switch, Route } from "react-router-dom"
-// import axios from "axios"
-import NavBar from "./pages/0.navBar"
-// import FirstPage from "./pages/1.firstPage"
-import Signup from "./pages/2.signUp"
-// import MainPage from "./pages/4.mainPage"
-import PostRead from "./pages/8.postRead"
-
 import "./App.css"
-const url =
-  process.env.REACT_APP_URL ||
-  "http://ec2-3-34-2-204.ap-northeast-2.compute.amazonaws.com"
+import PasswordChange from "./pages/11-2.pwdSection"
 
-function App() {
-  console.log(url)
+export default function App() {
   return (
     <>
-      <NavBar />
-      <div className="entire">
-        <Switch>
-          <Route path="/signup">
-            <Signup />
-          </Route>
-
-          <Route path="/postread">
-            <PostRead />
-          </Route>
-
-          {/* <Route path="/firstpage">
-            <FirstPage />
-          </Route> */}
-
-          {/* <Route exact path="/mainpage">
-            <MainPage />
-          </Route> */}
-
-          {/* <Route path="/">
-            {isLogin ? <Redirect to="/firstpage" /> : <Redirect to="/mainpage" />}
-          </Route> */}
-        </Switch>
-      </div>
+      <h1 style={{color: 'green'}}>Now testing page 11</h1>
+      <PasswordChange />
     </>
-  )
+  );
 }
-export default App
+
+// =====================================================================
+// import React, { useState } from "react"
+// import { Switch, Route } from "react-router-dom"
+// // import axios from "axios"
+// import NavBar from "./pages/0.navBar"
+// // import FirstPage from "./pages/1.firstPage"
+// import Signup from "./pages/2.signUp"
+// // import MainPage from "./pages/4.mainPage"
+// import PostRead from "./pages/8.postRead"
+
+// import "./App.css"
+// const url =
+//   process.env.REACT_APP_URL ||
+//   "http://ec2-3-34-2-204.ap-northeast-2.compute.amazonaws.com"
+
+// function App() {
+//   console.log(url)
+//   return (
+//     <>
+//       <NavBar />
+//       <div className="entire">
+//         <Switch>
+//           <Route path="/signup">
+//             <Signup />
+//           </Route>
+
+//           <Route path="/postread">
+//             <PostRead />
+//           </Route>
+
+//           {/* <Route path="/firstpage">
+//             <FirstPage />
+//           </Route> */}
+
+//           {/* <Route exact path="/mainpage">
+//             <MainPage />
+//           </Route> */}
+
+//           {/* <Route path="/">
+//             {isLogin ? <Redirect to="/firstpage" /> : <Redirect to="/mainpage" />}
+//           </Route> */}
+//         </Switch>
+//       </div>
+//     </>
+//   )
+// }
+// export default App
