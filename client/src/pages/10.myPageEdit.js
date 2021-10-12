46cbf700565f1a697f07e7928b1759a939f2b6dd
이상아 <lebikebihebi@gmail.com>
2021-10-12 20:47:36 +0900

    Design: 10페이지에 헤더, 네비게이션 추가

--- client/src/pages/10.myPageEdit.js
+++ client/src/pages/10.myPageEdit.js
@@ -1,11 +1,13 @@
+import Header from '../components/Header'
+import Navigation from '../components/Navigation'
 import MyPageEditSection from './10.myPageEditSection'
-// header, nav 추가
 
 export default function MyPageEdit () {
 
   return (
     <div>
-      <h1>MyPageEdit.js test</h1>
+      <Header />
+      <Navigation />
       <MyPageEditSection />
     </div>
   );
