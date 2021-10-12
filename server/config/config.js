729a014459487ebd509ddc7948bd356b012d11b1
emily-hong <emilyhong4659@gmail.com>
2021-10-13 03:40:27 +0900

    수정

--- server/config/config.js
+++ server/config/config.js
@@ -3,21 +3,21 @@ require("dotenv").config()
 module.exports = {
   development: {
     username: "root",
-    password: "",
+    password: "1234",
     database: "animalchat",
     host: "127.0.0.1",
     dialect: "mysql",
   },
   test: {
     username: "root",
-    password: "animalchat",
+    password: "1234",
     database: "animalchat",
     host: "127.0.0.1",
     dialect: "mysql",
   },
   production: {
     username: "root",
-    password: "animalchat",
+    password: "1234",
     database: "animalchat",
     host: "127.0.0.1",
     dialect: "mysql",
