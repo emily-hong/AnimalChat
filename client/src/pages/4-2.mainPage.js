7636c7dd80e38decd489965e1255b6dd5b111dfd
이상아 <lebikebihebi@gmail.com>
2021-10-12 15:31:29 +0900

    Chore: 최신버전의 origin/dev를 pull

--- client/src/pages/4-2.mainPage.js
+++ client/src/pages/4-2.mainPage.js
@@ -1,15 +1,8 @@
-<<<<<<< HEAD
-=======
 // import { Switch, Route } from "react-router-dom"
 import axios from "axios";
->>>>>>> dev
 import Header from "../components/Header"
 import Navigation from "../components/Navigation"
 import Boards from "./4-1.boards";
-<<<<<<< HEAD
-
-export default function MainPage() {
-=======
 // import Hamster from "./5-1.hamster"
 // import Chick from "./5-2.chick"
 // import Parrot from "./5-3.parrot"
@@ -18,7 +11,6 @@ export default function MainPage() {
 
 export default function MainPage(props) {
   //console.log(props)
->>>>>>> dev
   return (
     <div className="mainPage">
       <Header />
