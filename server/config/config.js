0819b5f5d82b5e3d9a49b8b4b8b89b95e235df49
emily-hong <emilyhong4659@gmail.com>
2021-10-13 13:23:30 +0900

    post수정중

--- server/config/config.js
+++ server/config/config.js
@@ -3,7 +3,7 @@ require("dotenv").config()
 module.exports = {
   development: {
     username: "root",
-    password: "animalchat",
+    password: "1234",
     database: "animalchat",
     host: "127.0.0.1",
     dialect: "mysql",
