c833f3e3da59db33f23ecb76dc2400d3dc924af2
kingshuny <nicecap77@gmail.com>
2021-10-13 13:30:37 +0900

    config.js -> config.json원복

--- server/models/index.js
+++ server/models/index.js
@@ -1,14 +1,14 @@
 "use strict"
-require("dotenv").config()
 const fs = require("fs")
 const path = require("path")
 const Sequelize = require("sequelize")
 const basename = path.basename(__filename)
 const env = process.env.NODE_ENV || "development"
-const config = require(__dirname + "/../config/config.js")[env]
+const config = require(__dirname + "/../config/config.json")[env]
 const db = {}
 
 let sequelize
+
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config)
 } else {
@@ -26,6 +26,7 @@ fs.readdirSync(__dirname)
       file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
     )
   })
+
   .forEach((file) => {
     const model = require(path.join(__dirname, file))(
       sequelize,
@@ -39,8 +40,6 @@ Object.keys(db).forEach((modelName) => {
     db[modelName].associate(db)
   }
 })
-
 db.sequelize = sequelize
 db.Sequelize = Sequelize
-
 module.exports = db
