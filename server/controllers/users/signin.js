948e0e2819c1fbdf771b2fb7adf3eaa60b80d5d0
kingshuny <nicecap77@gmail.com>
2021-10-14 13:04:59 +0900

    cors에러 핸들링13

--- server/controllers/users/signin.js
+++ server/controllers/users/signin.js
@@ -2,22 +2,25 @@ const { user } = require("../../models")
 // require("dotenv").config()
 const { generateAccessToken, sendAccessToken } = require("../tokenFunc")
 
-module.exports = async (req, res) => {
+module.exports = (req, res) => {
   //console.log(req.body) //{ id: 'kimcoding3', password: 'a123' }
-  let userFind = await user.findOne({
-    where: {
-      user_id: req.body.id,
-      password: req.body.password,
-    },
-  })
-
-  if (!userFind) {
-    res.status(404).send("invalid user")
-  } else {
-    //delete data.dataValues.password;
-    const accessToken = generateAccessToken(data.dataValues)
-    //console.log(data.dataValues)
-    //console.log(accessToken) //토큰 잘발급되고
-    sendAccessToken(res, accessToken, data.dataValues)
-  }
+  user
+    .findOne({
+      where: {
+        user_id: req.body.id,
+        password: req.body.password,
+      },
+    })
+    .then((data) => {
+      if (!data) {
+        res.status(404).send("invalid user")
+      } else {
+        delete data.dataValues.password
+        const accessToken = generateAccessToken(data.dataValues)
+        return sendAccessToken(res, accessToken)
+      }
+    })
+    .catch((err) => {
+      console.log(err)
+    })
 }
