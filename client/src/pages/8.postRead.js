4ad263ee91e477675ed5def53fee540ff95d8a95
이상아 <lebikebihebi@gmail.com>
2021-10-14 18:14:44 +0900

    Chore: reset 전

--- client/src/pages/8.postRead.js
+++ client/src/pages/8.postRead.js
@@ -2,19 +2,24 @@ import { useHistory } from "react-router-dom"
 import React, { useState, useEffect } from "react"
 import axios from "axios"
 import styled from "styled-components"
-import Header from "../components/Header"
-import Navigation from "../components/Navigation"
+// import Header from "../components/Header"
+// import Navigation from "../components/Navigation"
 import Comment from "./8.postRead-comment"
 
 // styled-component
 const Outer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
   width: 100vw;
   height: 100vh;
 `
 
 const Contents = styled.div`
-  background-color: #FEEFD5;
+  width: 100vw;
   height: 100vh;
+  background-color: #feefd5;
 `
 
 const PostReadSection = styled.div`
@@ -33,15 +38,10 @@ const PostReadSection = styled.div`
     width: 90%;
     margin-bottom: 2rem;
   }
-
-  & img {
-    border: 1px solid black;
-  }
 `
 
 const PostTitle = styled.div`
   display: flex;
-  justify-content: space-between;
   width: calc(100% - 2rem);
 `
 
@@ -49,17 +49,6 @@ const PostTitleLeft = styled.div`
   display: flex;
   flex-grow: 8;
   align-items: center;
-
-  & > img {
-    border: 1px solid red;
-    width: 3rem;
-    height: 3rem;
-    margin-right: 1rem;
-  }
-
-  & > h1 {
-    margin-right: 2rem;
-  }
 `
 
 const PostButtons = styled.div`
@@ -89,12 +78,8 @@ const PostButtons = styled.div`
 `
 
 const CommentSection = styled.div`
-  width: inherit;
-  padding: 1rem;
-
   & li {
     padding: 1rem;
-    margin: .5rem;
   }
 `
 
@@ -103,31 +88,16 @@ const PostComment = styled.div`
   justify-content: center;
   align-items: center;
   width: inherit;
-  margin-bottom: 2rem;
-
-  & .commentUsername {
-    display: flex;
-    font-size: .8rem;
-    & .username {
-      font-weight: bold;
-      margin-right: .5rem;
-    }
-    & .inputTitle {
-      margin-right: .5rem;
-    }
-  }
 
   & input {
-    width: 70%;
+    width: 80%;
     padding: 0.5rem;
     font-size: 1rem;
-    margin-right: 1rem;
   }
   & button {
     background-color: #419300;
     color: white;
-    margin-left: 1rem;
-    padding: .5rem;
+    height: 3rem;
   }
 `
 
@@ -167,113 +137,41 @@ const url =
 
 // 댓글 : content
 export default function PostRead(props) {
-  // title - 수정버튼 : history.push
-  console.log(props.curPost)
-  const history = useHistory()
   function editPostButton(event) {
-    history.push("/postedit")
   }
-  useEffect(() => {
-    handleButtonClick()
-  }, [])
 
-  // title - 삭제 :
   const deletePostButton = (event) => {
-    // alert("게시물을 삭제하시겠습니까?")
-    // 데이터베이스 게시물 삭제
-    if (window.confirm("게시물을 삭제하시겠습니까?")) {
-      axios({
-        url: url + "/postdelete",
-        method: "delete",
-
-        // data: {
-        //   // 삭제될 게시물 정보들
-        //   // user_id,
-        //   // post_title,
-        //   // post_content,
-        //   // post_img,
-        //   // animalcategory
-        // }
-        withCredentials: true
-      })
-      .then(() => {
-
-        alert("게시물 삭제 완료")
-        history.push("/mainpage") // 또는 board/{해당동물} 페이지로
-      })
-    }
   }
 
   // 뒤로 버튼
   const backButtonHandler = () => {
-    history.push("/")
   }
-
-  // 댓글
-  const [contentMsg, setContentMsg] = useState("") // 작성되어지는 댓글 (input)
-  const [cotentList, setContentList] = useState([]) // 댓글 목록
 
   // 댓글작성 버튼
   function handleButtonClick() {
-    // createdAt: new Date().toLocaleDateString("ko-kr"), // 여기 두 줄 뭔지 모르겠어요...
-    // updatedAt: new Date().toLocaleDateString("ko-kr"), // 일단 주석처리 합니다
-    axios({
-      url: url + "/commentsend",
-      method: "post",
-      data: {
-        post_id: props.curPost.id,
-        comment_user_id: props.userinfo.user_id,
-        comment_content: contentMsg,
-      },
-      withCredentials: true,
-    }).then((res) => {
-      // setContentList(res.data)
-      console.log("댓글작성완료")
-      handleButtonClick2()
-    })
   }
 
   function handleButtonClick2() {
-    console.log("handleButtonClick2")
-
-    axios({
-      url: url + "/commentlist",
-      method: "post",
-      data: {
-        post_id: props.curPost.id,
-        comment_user_id: props.userinfo.user_id,
-      },
-      withCredentials: true,
-    }).then((res) => setContentList(res.data))
-    console.log("handleButtonClick2끝")
-    console.log(cotentList)
   }
 
   // 댓글내용
   const handleChangeMsg = (event) => {
-    setContentMsg(event.target.value)
   }
-  console.log(cotentList)
 
   return (
     <Outer>
-      <Header />
-      <Navigation />
       {/* 내사진, 제목, 날짜, 버튼 */}
       <Contents>
         <PostReadSection>
           <PostTitle className="postTitle">
             <PostTitleLeft className="postTitle_left">
               <img className="profilePic" alt="프로필사진" />
-              <h1 className="title">{props.curPost.post_title}</h1>
-              <p>{props.curPost.updatedAt}</p>
+              <h1 className="title">{'제목테스트'}</h1>
+              <p>{'2000. 00. 00.'}</p>
             </PostTitleLeft>
-
             <PostButtons className="postTitle_right">
-
               <button className="editPost" onClick={editPostButton}>수정</button>
               <button className="deletePost" onClick={deletePostButton} >삭제</button>
-
             </PostButtons>
           </PostTitle>
 
@@ -281,26 +179,26 @@ export default function PostRead(props) {
           <div className="postPic">
             <img
               className="picture"
-              src={url + props.curPost.post_img}
+              src={null}
               alt="게시물 사진"
             />
           </div>
 
           {/* 게시물 내용 */}
-          <div className="postContent">{props.curPost.post_content}</div>
+          <div className="postContent">{'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla in porttitor lacus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Proin luctus augue in neque elementum, in egestas est condimentum. Vestibulum malesuada, magna at aliquam iaculis, dolor sem sagittis eros, interdum dapibus sapien neque quis.'}</div>
 
           {/* 뒤로 버튼 */}
           <BackButton className="backButton" onClick={backButtonHandler}>
             home
           </BackButton>
         </PostReadSection>
-        {/* 댓글 작성 */}
 
+        {/* 댓글 작성 */}
         <CommentSection>
           <PostComment className="postComment">
-            <div className="commentUsername">
-              <span className="username">{props.userinfo.user_id}</span>
-              <span className="inputTitle">댓글달기</span>
+            <div>
+              <span>username</span>
+              <span>댓글달기</span>
             </div>
             <input
               className="inputComment"
@@ -313,12 +211,168 @@ export default function PostRead(props) {
 
           {/* 댓글 목록 */}
           <CommentList className="commentsList">
-            {cotentList.map((content) => (
-              <Comment content={content} />
-            ))}
+            <Comment />
+            <Comment />
+            <Comment />
+            <Comment />
+            <Comment />
+            <Comment />
           </CommentList>
         </CommentSection>
       </Contents>
     </Outer>
   )
 }
+
+// export default function PostRead(props) {
+//   // title - 수정버튼 : history.push
+//   console.log(props.curPost)
+//   const history = useHistory()
+//   function editPostButton(event) {
+//     history.push("/postedit")
+//   }
+//   useEffect(() => {
+//     handleButtonClick()
+//   }, [])
+
+//   // title - 삭제 :
+//   const deletePostButton = (event) => {
+//     // alert("게시물을 삭제하시겠습니까?")
+//     // 데이터베이스 게시물 삭제
+//     if (window.confirm("게시물을 삭제하시겠습니까?")) {
+//       axios({
+//         url: url + "/postdelete",
+//         method: "delete",
+
+//         // data: {
+//         //   // 삭제될 게시물 정보들
+//         //   // user_id,
+//         //   // post_title,
+//         //   // post_content,
+//         //   // post_img,
+//         //   // animalcategory
+//         // }
+//         withCredentials: true
+//       })
+//       .then(() => {
+
+//         alert("게시물 삭제 완료")
+//         history.push("/mainpage") // 또는 board/{해당동물} 페이지로
+//       })
+//     }
+//   }
+
+//   // 뒤로 버튼
+//   const backButtonHandler = () => {
+//     history.push("/")
+//   }
+
+//   // 댓글
+//   const [contentMsg, setContentMsg] = useState("") // 작성되어지는 댓글 (input)
+//   const [cotentList, setContentList] = useState([]) // 댓글 목록
+
+//   // 댓글작성 버튼
+//   function handleButtonClick() {
+//     // createdAt: new Date().toLocaleDateString("ko-kr"), // 여기 두 줄 뭔지 모르겠어요...
+//     // updatedAt: new Date().toLocaleDateString("ko-kr"), // 일단 주석처리 합니다
+//     axios({
+//       url: url + "/commentsend",
+//       method: "post",
+//       data: {
+//         post_id: props.curPost.id,
+//         comment_user_id: props.userinfo.user_id,
+//         comment_content: contentMsg,
+//       },
+//       withCredentials: true,
+//     }).then((res) => {
+//       // setContentList(res.data)
+//       console.log("댓글작성완료")
+//       handleButtonClick2()
+//     })
+//   }
+
+//   function handleButtonClick2() {
+//     console.log("handleButtonClick2")
+
+//     axios({
+//       url: url + "/commentlist",
+//       method: "post",
+//       data: {
+//         post_id: props.curPost.id,
+//         comment_user_id: props.userinfo.user_id,
+//       },
+//       withCredentials: true,
+//     }).then((res) => setContentList(res.data))
+//     console.log("handleButtonClick2끝")
+//     console.log(cotentList)
+//   }
+
+//   // 댓글내용
+//   const handleChangeMsg = (event) => {
+//     setContentMsg(event.target.value)
+//   }
+//   console.log(cotentList)
+
+//   return (
+//     <Outer>
+//       {/* 내사진, 제목, 날짜, 버튼 */}
+//       <Contents>
+//         <PostReadSection>
+//           <PostTitle className="postTitle">
+//             <PostTitleLeft className="postTitle_left">
+//               <img className="profilePic" alt="프로필사진" />
+//               <h1 className="title">{props.curPost.post_title}</h1>
+//               <p>{props.curPost.updatedAt}</p>
+//             </PostTitleLeft>
+
+//             <PostButtons className="postTitle_right">
+
+//               <button className="editPost" onClick={editPostButton}>수정</button>
+//               <button className="deletePost" onClick={deletePostButton} >삭제</button>
+
+//             </PostButtons>
+//           </PostTitle>
+
+//           {/* 게시물 사진 */}
+//           <div className="postPic">
+//             <img
+//               className="picture"
+//               src={url + props.curPost.post_img}
+//               alt="게시물 사진"
+//             />
+//           </div>
+
+//           {/* 게시물 내용 */}
+//           <div className="postContent">{props.curPost.post_content}</div>
+
+//           {/* 뒤로 버튼 */}
+//           <BackButton className="backButton" onClick={backButtonHandler}>
+//             home
+//           </BackButton>
+//         </PostReadSection>
+//         {/* 댓글 작성 */}
+
+//         <CommentSection>
+//           <PostComment className="postComment">
+//             <div>{props.userinfo.user_id} 댓글달기:</div>
+//             <input
+//               className="inputComment"
+//               type="text"
+//               placeholder="댓글을 작성해주세요."
+//               onChange={handleChangeMsg}
+//             />
+//             <button onClick={handleButtonClick}>작성</button>
+//           </PostComment>
+
+//           {/* 댓글 목록 */}
+//           <CommentList className="commentsList">
+//             {cotentList.map((content) => (
+//               <Comment content={content} />
+//             ))}
+//           </CommentList>
+//         </CommentSection>
+//       </Contents>
+//     </Outer>
+//   )
+// }
+
