1459793b8ac7df4d60fb2e2e74e546e86a60a976
seonhwa oh <seonhwa_0713@naver.com>
2021-12-06 13:04:07 +0900

    Feat:like 테이블 추가

---
+++ server/models/like.js
@@ -0,0 +1,26 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class like extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      models.like.belongsTo(models.user, {foreignKey: "user_id", targetKey: "id"})
+      models.like.belongsTo(models.comment, {foreignKey: "comment_id", targetKey: "id"})
+    }
+  };
+  like.init({
+    user_id: DataTypes.STRING,
+    comment_id: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'like',
+  });
+  return like;
+};
