9cf9c71ab1a9cb522daba2cc4f1fd62dd0d3fcc7
emily-hong <emilyhong4659@gmail.com>
2021-12-29 14:50:22 +0900

    client url수정2

--- client/src/pages/4.mainPage.js
+++ client/src/pages/4.mainPage.js
@@ -15,7 +15,7 @@ const Outer = styled.div`
 `
 const url =
     process.env.REACT_APP_URL ||
-    "http://ec2-52-79-218-114.ap-northeast-2.compute.amazonaws.com"
+    "http://ec2-3-35-9-246.ap-northeast-2.compute.amazonaws.com"
 
 export default function MainPage(props) {
     useEffect(() => {
--- client/src/pages/6.post.js
+++ client/src/pages/6.post.js
@@ -226,7 +226,7 @@ const PostCancelBtnMargin = styled.button`
 
 const url =
     process.env.REACT_APP_URL ||
-    "http://ec2-52-79-218-114.ap-northeast-2.compute.amazonaws.com"
+    "http://ec2-3-35-9-246.ap-northeast-2.compute.amazonaws.com"
 
 export const Post = (props) => {
     const history = useHistory()
