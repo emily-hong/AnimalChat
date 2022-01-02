5282892cdcd942d2e47e30219475b84c146d7ac3
seonhwa oh <seonhwa_0713@naver.com>
2022-01-02 22:28:10 +0900

    Chore:server/config 수정ㅇ

--- server/config/config.js
+++ server/config/config.js
@@ -1,25 +1,27 @@
 require("dotenv").config();
+const env = process.env;
 
-module.exports = {
-  development: {
-    username: process.env.DB_ID,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+  const development = {
+    username: env.DB_ID,
+    password: env.DB_PASSWORD,
+    database: env.DB_NAME,
     host:"127.0.0.1",
     dialect: "mysql"
   },
-  test: {
-    username: process.env.DB_ID,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+  const test = {
+    username: env.DB_ID,
+    password: env.DB_PASSWORD,
+    database: env.DB_NAME,
     host:"127.0.0.1",
     dialect: "mysql"
   },
-  production: {
-    username: process.env.DB_ID,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+  const production = {
+    username: env.DB_ID,
+    password: env.DB_PASSWORD,
+    database: env.DB_NAME,
     host:"127.0.0.1",
     dialect: "mysql"
   }
-}
+
+
+module.exports = { development, production, test };
