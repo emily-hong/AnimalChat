ad50a8ceb8fb4fbe47b5e41b9ec93de357ca5e2c
seonhwa oh <seonhwa_0713@naver.com>
2022-01-03 00:01:21 +0900

    Chore:server/config 수정2

--- server/config/config.js
+++ server/config/config.js
@@ -7,21 +7,20 @@ const env = process.env;
     database: env.DB_NAME,
     host:"127.0.0.1",
     dialect: "mysql"
-  },
+  };
   const test = {
     username: env.DB_ID,
     password: env.DB_PASSWORD,
     database: env.DB_NAME,
     host:"127.0.0.1",
     dialect: "mysql"
-  },
+  };
   const production = {
     username: env.DB_ID,
     password: env.DB_PASSWORD,
     database: env.DB_NAME,
     host:"127.0.0.1",
     dialect: "mysql"
-  }
-
+  };
 
 module.exports = { development, production, test };
