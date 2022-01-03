af9ee24777a2b8babe79d23d2c5c7e98c18ee020
emily-hong <emilyhong4659@gmail.com>
2022-01-03 22:02:04 +0900

    test

--- client/src/pages/5-2.chick.js
+++ client/src/pages/5-2.chick.js
@@ -6,10 +6,9 @@ import React, { useEffect } from "react"
 import axios from "axios"
 import Footer from "../components/Footer"
 
-let url = process.env.REACT_APP_API_URL
-if(!url) url = "http://ec2-13-125-84-11.ap-northeast-2.compute.amazonaws.com"
-// let url = "http://ec2-13-125-84-11.ap-northeast-2.compute.amazonaws.com"
-
+// let url = process.env.REACT_APP_API_URL
+// if(!url) url = "http://ec2-13-209-35-203.ap-northeast-2.compute.amazonaws.com"
+let url = "http://ec2-13-209-35-203.ap-northeast-2.compute.amazonaws.com"
 export default function Chick(props) {
 
     useEffect(() => {
